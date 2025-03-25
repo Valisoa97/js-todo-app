ae85b1e8369ab236f17a69526b30f65e6d1a163d
fandresena-sowell <89010878+fandresena-sowell@users.noreply.github.com>
2025-03-25 15:54:30 +0300

    feat: enhance task creation and rendering functionality
    Refactor the task creation process to include input validation and 
    improve the rendering of tasks in the UI. The `createTask` function 
    now validates the input before creating a new task and adds it to 
    the task list. Introduce a `templateTask` function to generate 
    HTML for each task, and implement a `renderTasks` function to 
    display tasks dynamically. Set up event listeners in a dedicated 
    function for better organization and maintainability.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import "../styles/main.css"
+import { Task, TaskManager } from "./models/tasks"
 
 // DOM element references
 const addTaskBtn = document.getElementById("open-popup-btn")
@@ -6,7 +7,8 @@ const closePopupBtn = document.getElementById("close-popup-btn")
 const createTaskBtn = document.getElementById("create-task-btn")
 const popupContainer = document.getElementById("popup-container")
 const createInput = document.getElementById("create-input")
-
+const todoList = document.getElementById("todo-list")
+const emptyTaskImage = document.querySelector(".empty-task-image")
 // Core functions
 function openPopup() {
   document.body.style.overflow = "hidden"
@@ -20,29 +22,74 @@ function closePopup() {
 }
 
 function createTask() {
-  const task = createInput.value
-  console.log("create task", task)
-  // TODO: Add task to the list
+  const title = createInput.value
+  // Validate input
+  if (!title) {
+    return
+  }
+  const newTask = new Task(TaskManager.generateId(), title)
+  TaskManager.addTask(newTask)
   closePopup()
+  renderTasks()
+}
+
+function templateTask(task) {
+  return `
+  <li id="task-${task.id}" class="flex items-center pb-3 mb-2 border-b border-[#6C63FF]">
+    <div class="checkbox-container mr-3 cursor-pointer">
+      <input type="checkbox" id="todo-${task.id}" class="hidden" />
+      <label for="todo-${task.id}" class="block w-6 h-6 cursor-pointer">
+        <img
+          src="src/assets/svg/checkbox-unchecked.svg"
+          class="checkbox-unchecked"
+          alt="Unchecked"
+        />
+        <img
+          src="src/assets/svg/checkbox-checked.svg"
+          class="checkbox-checked hidden"
+          alt="Checked"
+        />
+      </label>
+    </div>
+    <label for="todo-${task.id}" class="text-xl font-medium cursor-pointer">
+      ${task.title}
+    </label>
+  </li>
+  `
+}
+
+function renderTasks() {
+  todoList.innerHTML = ""
+  if (TaskManager.getTasks().length === 0) {
+    emptyTaskImage.style.display = "block"
+  } else {
+    emptyTaskImage.style.display = "none"
+    TaskManager.getTasks().forEach(task => {
+      todoList.innerHTML += templateTask(task)
+    })
+  }
 }
 
 // Event listeners
-addTaskBtn.addEventListener("click", openPopup)
-closePopupBtn.addEventListener("click", closePopup)
-createTaskBtn.addEventListener("click", createTask)
+function setupEventListeners() {
+  addTaskBtn.addEventListener("click", openPopup)
+  closePopupBtn.addEventListener("click", closePopup)
+  createTaskBtn.addEventListener("click", createTask)
+  popupContainer.addEventListener("click", event => {
+    const createForm = document.getElementById("create-form")
+    if (!createForm.contains(event.target) && event.target !== addTaskBtn) {
+      closePopup()
+    }
+  })
 
-popupContainer.addEventListener("click", event => {
-  const createForm = document.getElementById("create-form")
-  if (!createForm.contains(event.target) && event.target !== addTaskBtn) {
-    closePopup()
-  }
-})
+  // Add keyboard support
+  createInput.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+      createTask()
+    } else if (event.key === "Escape") {
+      closePopup()
+    }
+  })
+}
 
-// Add keyboard support
-createInput.addEventListener("keydown", event => {
-  if (event.key === "Enter") {
-    createTask()
-  } else if (event.key === "Escape") {
-    closePopup()
-  }
-})
+setupEventListeners()
